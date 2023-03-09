a6261dec96c09afeb3fbc019f3ae4313d0e1ed17
fnaufalh <fnaufalh@gmail.com>
2023-03-09 07:41:53 +0700

    check a frame with name is exist or not

--- code.ts
+++ code.ts
@@ -1,20 +1,31 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+const nodes = {
+  fullPage: 0,
+};
+const page = figma.currentPage;
+let frame: FrameNode | null = null;
+let count = 0;
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
-
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+for (const node of page.children) {
+  if (node.type === "FRAME" && node.name.includes("Full Page")) {
+    frame = node as FrameNode;
+    nodes.fullPage += 1;
+  }
+  count += 1;
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+
+if (frame) {
+  console.log(nodes.fullPage, count);
+}
+
+// for (let i = 0; i < numberOfRectangles; i++) {
+//   const rect = figma.createRectangle();
+//   rect.x = i * 150;
+//   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//   figma.currentPage.appendChild(rect);
+//   nodes.push(rect);
+// }
+// figma.currentPage.selection = nodes;
+// figma.viewport.scrollAndZoomIntoView(nodes);
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.
