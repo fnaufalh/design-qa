7d5722ed785edab0f667c66a472996403da94420
Fathin Naufal Hawi <naufal@Fathins-MacBook-Air.local>
2023-03-10 20:24:47 +0700

    try to get instance, still error

--- code.ts
+++ code.ts
@@ -2,31 +2,35 @@ const nodes = {
   fullPage: 0,
 };
 const page = figma.currentPage;
+const checklist = "design-qa/checklist";
+const componentSet = figma.currentPage.findOne(
+  (node) => node.type === "COMPONENT_SET" && node.key === checklist
+) as ComponentSetNode;
+// const instances = figma.currentPage.findAll(
+//   (node) => node.type === "INSTANCE" && node.mainComponent?.key === checklist
+// );
 let frame: FrameNode | null = null;
-let count = 0;
-
+let variantIdentifier = "Passed=yes";
+const instance = figma.createComponent();
 for (const node of page.children) {
   if (node.type === "FRAME" && node.name.includes("Full Page")) {
-    frame = node as FrameNode;
-    nodes.fullPage += 1;
+    frame = node;
+    instance.setMainComponent(componentSet);
+    // let instance = instances.find((node) => node.variantIden === variantIdentifier)
   }
-  count += 1;
 }
 
-if (frame) {
-  console.log(nodes.fullPage, count);
-}
-
-// for (let i = 0; i < numberOfRectangles; i++) {
-//   const rect = figma.createRectangle();
-//   rect.x = i * 150;
-//   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-//   figma.currentPage.appendChild(rect);
-//   nodes.push(rect);
+// if (frame) {
+//   let isDesignQa = figma.root.findOne(
+//     (node) => node.type === "PAGE" && node.name === "Design QA"
+//   ) as PageNode;
+//   if (isDesignQa) {
+//     figma.currentPage = isDesignQa;
+//   } else {
+//     let designQa = figma.createPage();
+//     designQa.name = "Design QA";
+//     // figma.currentPage = designQa;
+//   }
 // }
-// figma.currentPage.selection = nodes;
-// figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
 figma.closePlugin();
